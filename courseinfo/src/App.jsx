401fd613594f356c69ad3d9234f3cdccbb9d036f
Carlos Romero <c6099020@gmail.com>
2023-11-24 20:23:00 -0500

    Exercise 3 & 4: Parts as JavaScript objects

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -39,12 +39,6 @@ const App = () => {
     {name: 'Using props to pass data', exercises: 7  },
     {name: 'State of a component',     exercises: 14 }
   ]
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
 
   return (
     <div>
