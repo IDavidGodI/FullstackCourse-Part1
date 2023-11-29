0e15245532b3b921ca35a6c1ef1a6c74532eb0b1
Carlos Romero <c6099020@gmail.com>
2023-11-28 22:09:19 -0500

    Exercise 11: Unicafe step 6

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -11,18 +11,25 @@ const Statistics = ({good, neutral, bad}) => {
   const positive = good/all || 0;
 
   return (
-    <div>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={all}/>
-      <StatisticLine text="average" value={average}/>
-      <StatisticLine text="positive" value={(positive*100)+"%"}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={(positive*100)+"%"}/>
+      </tbody>
+    </table>
   )
 }
 
-const StatisticLine = ({text, value})=>(<p>{text} {value}</p>)
+const StatisticLine = ({text, value})=>(
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 
 
